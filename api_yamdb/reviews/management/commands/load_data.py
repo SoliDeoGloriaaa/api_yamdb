230082d7348c884865e5be94dbb2c09aa8b2fc97
Дмитрий Емельянов <thl00@rambler.ru>
2023-02-22 15:01:28 +0300

    22.02.2023 csv

--- api_yamdb/reviews/management/commands/load_data.py
+++ api_yamdb/reviews/management/commands/load_data.py
@@ -8,27 +8,34 @@ from api_yamdb.reviews.models import (
 )
 
 
-TABLES = {
-    User: 'users.csv',
-    Category: 'category.csv',
-    Genre: 'genre.csv',
-    GenreTitle: 'genre_title.csv',
-    Title: 'titles.csv',
-    Review: 'review.csv',
-    Comment: 'comments.csv',
-}
+TABLES = (
+    (User, 'static/data/users.csv'),
+    (Category, 'static/data/category.csv'),
+    (Genre, 'static/data/genre.csv'),
+    (GenreTitle, 'static/data/genre_title.csv'),
+    (Title, 'static/data/titles.csv'),
+    (Review, 'static/data/review.csv'),
+    (Comment, 'static/data/comments.csv')
+)
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        for model, base in TABLES.items():
-            with open(
-                f'{settings.BASE_DIR}/static/data/{base}',
-                'r',
-                encoding='utf-8'
-            ) as csv_file:
-                reader = csv.DictReader(csv_file)
-                model.objects.bulk_create(
-                    model(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Все данные загружены'))
+        try:
+            for model, base in TABLES:
+                with open(
+                    f'{settings.BASE_DIR}/static/data/{base}',
+                    encoding='utf8'
+                ) as csv_file:
+                    reader = csv.DictReader(csv_file)
+                    for row_dict in reader:
+                        model_date = model(data=row_dict)
+                        if model_date.is_valid():
+                            model_date.save()
+                    self.stdout.write(self.style.SUCCESS(
+                        f'Данные из файла {base} успешно загружены'
+                    ))
+            self.stdout.write(self.style.SUCCESS('Все файлы загружены'))
+        except Exception as eroor:
+            raise Exception(f'Ошибка {eroor}')
